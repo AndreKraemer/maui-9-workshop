2d8301e010d54c20d46bc45dd19b50d9341c97a3
Andre Kraemer <andre@andrekraemer.de>
2024-11-24 17:32:44 +0100

    11: Ende

---
+++ src/DontLetMeExpire/DontLetMeExpire/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,67 @@
+﻿using DontLetMeExpire.Models;
+using DontLetMeExpire.Services;
+using System.Collections.ObjectModel;
+
+namespace DontLetMeExpire.ViewModels;
+
+public class ItemsViewModel : ViewModelBase
+{
+    private readonly IItemService _itemService;
+    private readonly IStorageLocationService _storageLocationService;
+
+    public ObservableCollection<Item> Items { get; private set; } = new ObservableCollection<Item>();
+
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+
+    public ItemsViewModel(IItemService itemService, IStorageLocationService storageLocationService)
+    {
+        _itemService = itemService;
+        _storageLocationService = storageLocationService;
+    }
+
+
+    public async Task InitializeAsync(string displayMode, string locationId)
+    {
+        IEnumerable<Item> items; // = await _itemService.GetAsync();
+
+        switch (displayMode)
+        {
+            case "Stock":
+                items = await _itemService.GetAsync();
+                Title = "Mein Vorrat";
+                break;
+            case "ExpiringSoon":
+                items = await _itemService.GetExpiresSoonAsync();
+                Title = "Läuft bald ab";
+                break;
+            case "ExpiresToday":
+                items = await _itemService.GetExpiresTodayAsync();
+                Title = "Läuft heute ab";
+                break;
+            case "Expired":
+                Title = "Abgelaufen";
+                items = await _itemService.GetExpiredAsync();
+                break;
+            case "Location":
+                items = await _itemService.GetByLocationAsync(locationId);
+                var location = await _storageLocationService.GetByIdAsync(locationId);
+                Title = location?.Name ?? "Ort";
+                break;
+            default:
+                Title = "Mein Vorrat";
+                items = await _itemService.GetAsync();
+                break;
+        }
+        Items.Clear();
+        foreach (var item in items)
+        {
+            Items.Add(item);
+        }
+    }
+}
--- src/DontLetMeExpire/DontLetMeExpire/ViewModels/MainViewModel.cs
+++ src/DontLetMeExpire/DontLetMeExpire/ViewModels/MainViewModel.cs
@@ -44,11 +44,51 @@ public class MainViewModel : ViewModelBase
     }
 
     private IEnumerable<StorageLocationWithItemCount> _storageLocations;
-    public IEnumerable<StorageLocationWithItemCount> StorageLocations 
+    public IEnumerable<StorageLocationWithItemCount> StorageLocations
     {
-        get => _storageLocations; 
-        private set => SetProperty(ref _storageLocations, value); 
+        get => _storageLocations;
+        private set => SetProperty(ref _storageLocations, value);
     }
+
+    public ICommand NavigateToStockCommand { get; }
+    private async Task NavigateToStock()
+    {
+        await NavigateToItemsPage("Stock");
+    }
+    public ICommand NavigateToExpiringSoonCommand { get; }
+    private async Task NavigateToExpiringSoon()
+    {
+        await NavigateToItemsPage("ExpiringSoon");
+    }
+    public ICommand NavigateToExpiresTodayCommand { get; }
+    private async Task NavigateToExpiresToday()
+    {
+        await NavigateToItemsPage("ExpiresToday");
+    }
+    public ICommand NavigateToExpiredCommand { get; }
+    private async Task NavigateToExpired()
+    {
+        await NavigateToItemsPage("Expired");
+    }
+
+    public ICommand NavigateToLocationCommand { get; set; }
+    private async Task NavigateToLocation(string locationId)
+    {
+        await NavigateToItemsPage("Location", locationId);
+    }
+
+
+    private async Task NavigateToItemsPage(string displayMode, string? locationId = null)
+    {
+        var navigationParams = new Dictionary<string, object>
+            {
+                { "DisplayMode", displayMode },
+                { "LocationId", locationId }
+            };
+        await _navigationService.GoToAsync(nameof(ItemsPage), navigationParams);
+    }
+
+
 
     public ICommand NavigateToAddItemCommand { get; }
 
@@ -62,6 +102,11 @@ public class MainViewModel : ViewModelBase
         _navigationService = navigationService;
         _itemService = itemService;
         _storageLocationService = storageLocationService;
+        NavigateToStockCommand = new Command(async () => await NavigateToStock());
+        NavigateToExpiringSoonCommand = new Command(async () => await NavigateToExpiringSoon());
+        NavigateToExpiresTodayCommand = new Command(async () => await NavigateToExpiresToday());
+        NavigateToExpiredCommand = new Command(async () => await NavigateToExpired());
+        NavigateToLocationCommand = new Command<string>(async (locationId) => await NavigateToLocation(locationId));
         NavigateToAddItemCommand = new Command(async () => await NavigateToAddItem());
     }
 
